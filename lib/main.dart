72f07aa51913573c69043084cf13de17ddfac31d
Sumit Pal <sumitpal.1996@gmail.com>
2022-01-20 20:44:36 +0530

    assets updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wally_the_stupid/UI/ui.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,31 +17,34 @@ class MyApp extends StatelessWidget {
       home: Splash(),
     );
   }
+
+  appLightTheme() => ThemeData(
+    accentColor: UI.appBackDarkColor.withOpacity(0.75),
+    appBarTheme: AppBarTheme(
+      color: Colors.transparent,
+      
+    )
+  );
 }
 
 class Splash extends StatefulWidget {
-  Splash({Key key,}) : super(key: key);
+  Splash({
+    Key key,
+  }) : super(key: key);
 
-  
   @override
   _SplashState createState() => _SplashState();
 }
 
 class _SplashState extends State<Splash> {
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        
-      ),
+      appBar: AppBar(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            
-          ],
+          children: <Widget>[],
         ),
       ),
     );
