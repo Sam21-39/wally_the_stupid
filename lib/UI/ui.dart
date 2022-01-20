5bea1e5a89bf12e67aaf47db1ff6fbae0e4ea7f1
Sumit Pal <sumitpal.1996@gmail.com>
2022-01-20 20:23:24 +0530

    Create ui.dart

---
+++ lib/UI/ui.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class UI {
+  static final appBackColor = Color(0xff8BAFC3);
+  static final appButtonColor = Color(0xff11948C);
+  static final appBackDarkColor = Color(0xff243844);
+}
