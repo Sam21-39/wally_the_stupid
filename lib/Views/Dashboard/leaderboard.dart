46ddcc769e34ca19be82285ec3538d60747d2437
Sumit Pal <sumitpal.1996@gmail.com>
2022-01-22 13:03:59 +0530

    Update leaderboard.dart

--- lib/Views/Dashboard/leaderboard.dart
+++ lib/Views/Dashboard/leaderboard.dart
@@ -16,87 +16,109 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return StreamBuilder(
-      stream: FirebaseFirestore.instance.collection('Leaderboard').snapshots(),
-      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-        if (snapshot.hasData) {
-          final data = snapshot.data.docs;
-          final leaderList = [];
-          data.forEach((e) {
-            leaderList.add(e);
-          });
+    return Column(
+      children: [
+        SizedBox(
+          height: size.height * 0.02,
+        ),
+        Text(
+          'LeaderBoard',
+          style: UI.appText.copyWith(
+            color: UI.appErrorColor,
+            decoration: TextDecoration.underline,
+            fontSize: 24.0,
+          ),
+        ),
+        SizedBox(
+          height: size.height * 0.01,
+        ),
+        Expanded(
+          child: StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection('Leaderboard')
+                .snapshots(),
+            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+              if (snapshot.hasData) {
+                final data = snapshot.data.docs;
+                final leaderList = [];
+                data.forEach((e) {
+                  leaderList.add(e);
+                });
 
-          return ListView.builder(
-            itemCount: leaderList.length,
-            itemBuilder: (context, index) {
-              return Card(
-                elevation: 6.0,
-                borderOnForeground: true,
-                color: UI.appErrorColor,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12.0),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '${(index + 1).toString()}.',
-                        style: UI.appText,
+                return ListView.builder(
+                  itemCount: leaderList.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      elevation: 6.0,
+                      borderOnForeground: true,
+                      color: UI.appErrorColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
                       ),
-                      Container(
-                        // color: Colors.amber,
-                        width: size.width * 0.4,
-                        child: FutureBuilder(
-                            future: FirebaseFirestore.instance
-                                .collection('User')
-                                .doc(leaderList[index]['uid'])
-                                .get(),
-                            builder: (context, snaps) {
-                              if (snaps.hasData) {
-                                final data = snaps.data;
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              '${(index + 1).toString()}.',
+                              style: UI.appText,
+                            ),
+                            Container(
+                              // color: Colors.amber,
+                              width: size.width * 0.4,
+                              child: FutureBuilder(
+                                  future: FirebaseFirestore.instance
+                                      .collection('User')
+                                      .doc(leaderList[index]['uid'])
+                                      .get(),
+                                  builder: (context, snaps) {
+                                    if (snaps.hasData) {
+                                      final data = snaps.data;
 
-                                return Text(
-                                  (data as DocumentSnapshot<Map>)['name'],
-                                  style: UI.appText.copyWith(
-                                    color: data['name'] ==
-                                            FirebaseAuth.instance.currentUser
-                                                ?.displayName
-                                        ? UI.appHighLightColor
-                                        : Colors.white,
-                                  ),
-                                  overflow: TextOverflow.ellipsis,
-                                );
-                              } else {
-                                return Center(
-                                  child: CircularProgressIndicator(
-                                    valueColor: AlwaysStoppedAnimation(
-                                      UI.appBackDarkColor,
-                                    ),
-                                  ),
-                                );
-                              }
-                            }),
+                                      return Text(
+                                        (data as DocumentSnapshot<Map>)['name'],
+                                        style: UI.appText.copyWith(
+                                          color: data['name'] ==
+                                                  FirebaseAuth.instance
+                                                      .currentUser?.displayName
+                                              ? UI.appHighLightColor
+                                              : Colors.white,
+                                        ),
+                                        overflow: TextOverflow.ellipsis,
+                                      );
+                                    } else {
+                                      return Center(
+                                        child: CircularProgressIndicator(
+                                          valueColor: AlwaysStoppedAnimation(
+                                            UI.appBackDarkColor,
+                                          ),
+                                        ),
+                                      );
+                                    }
+                                  }),
+                            ),
+                            Text(
+                              leaderList[index]['score'].toString(),
+                              style: UI.appText,
+                            ),
+                          ],
+                        ),
                       ),
-                      Text(
-                        leaderList[index]['score'].toString(),
-                        style: UI.appText,
-                      ),
-                    ],
+                    );
+                  },
+                );
+              } else {
+                return CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation(
+                    UI.appBackDarkColor,
                   ),
-                ),
-              );
+                );
+              }
             },
-          );
-        } else {
-          return CircularProgressIndicator(
-            valueColor: AlwaysStoppedAnimation(
-              UI.appBackDarkColor,
-            ),
-          );
-        }
-      },
+          ),
+        ),
+      ],
     );
   }
 }
