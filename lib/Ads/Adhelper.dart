051b1cba73c7348e48c373181b72c34782062c15
Sumit Pal <sumitpal.1996@gmail.com>
2022-01-28 23:17:55 +0530

    adhelper updated

--- lib/Ads/Adhelper.dart
+++ lib/Ads/Adhelper.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-4026417628443700/9197223812';
       // } else if (Platform.isIOS) {
       //   return 'ca-app-pub-3940256099942544/2934735716';
     } else {
