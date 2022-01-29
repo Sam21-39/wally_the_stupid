43c163dfefaf4064debca101d2394994bbdb90b7
Sumit Pal <sumitpal.1996@gmail.com>
2022-01-29 14:24:36 +0530

    Update Adhelper.dart

--- lib/Ads/Adhelper.dart
+++ lib/Ads/Adhelper.dart
@@ -5,7 +5,7 @@ import 'package:flutter/services.dart';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      PlatformViewsService.synchronizeToNativeViewHierarchy(false);
+      PlatformViewsService.synchronizeToNativeViewHierarchy(false);// this is added to stop flickering the adview on page change
       return 'ca-app-pub-4026417628443700/9197223812';
       // } else if (Platform.isIOS) {
       //   return 'ca-app-pub-3940256099942544/2934735716';
