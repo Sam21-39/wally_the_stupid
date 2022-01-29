7c8e22d7cebf047b50e1d56e17da89248d6ea7d6
Sumit Pal <sumitpal.1996@gmail.com>
2022-01-29 13:21:05 +0530

    Update tap.dart

--- lib/Views/TapPage/tap.dart
+++ lib/Views/TapPage/tap.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:wally_the_stupid/Model/challenge.dart';
 import 'package:wally_the_stupid/UI/ui.dart';
 
 class TapPage extends StatefulWidget {
-  const TapPage({Key? key}) : super(key: key);
+  final Challenge? challenge;
+  const TapPage({
+    Key? key,
+    this.challenge,
+  }) : super(key: key);
 
   @override
   _TapPageState createState() => _TapPageState();
 }
 
 class _TapPageState extends State<TapPage> {
-  int no = 5;
+  // int no = 5;
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -25,7 +30,7 @@ class _TapPageState extends State<TapPage> {
               height: size.height * 0.1,
             ),
             Text(
-              'Let\'s try to make the no. to 0 by tapping the screen',
+              widget.challenge?.prompt ?? 'Challenge',
               style: UI.appText.copyWith(
                 fontSize: 30.0,
                 fontWeight: FontWeight.w900,
@@ -36,7 +41,7 @@ class _TapPageState extends State<TapPage> {
               height: size.height * 0.1,
             ),
             Text(
-              no.toString(),
+              widget.challenge?.start.toString() ?? '0',
               style: UI.appText.copyWith(
                 fontSize: 64.0,
                 fontWeight: FontWeight.w900,
