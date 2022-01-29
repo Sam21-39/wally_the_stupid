ce945e4bade0c739072f100adec17a26acf1db21
Sumit Pal <sumitpal.1996@gmail.com>
2022-01-29 19:08:02 +0530

    Update tap.dart

--- lib/Views/TapPage/tap.dart
+++ lib/Views/TapPage/tap.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:wally_the_stupid/Model/challenge.dart';
 import 'package:wally_the_stupid/UI/ui.dart';
 
@@ -14,44 +17,107 @@ class TapPage extends StatefulWidget {
 }
 
 class _TapPageState extends State<TapPage> {
-  // int no = 5;
+  num no = 0;
+  num time = 0;
+  late Timer timer;
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      no = widget.challenge?.start as num;
+    });
+    Future.delayed(Duration(milliseconds: 800)).then(
+      (value) => timer = Timer.periodic(
+        Duration(milliseconds: 1000),
+        (timer) {
+          time += 1;
+          setState(() {});
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
-      body: Container(
-        width: size.width,
-        height: size.height,
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            SizedBox(
-              height: size.height * 0.1,
+    return GestureDetector(
+      onTap: () => setState(() => no += 1),
+      onDoubleTap: () => setState(() => no -= 2),
+      child: Scaffold(
+        backgroundColor: Theme.of(context).backgroundColor,
+        body: Container(
+          width: size.width,
+          height: size.height,
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: size.height * 0.1,
+                ),
+                Text(
+                  widget.challenge?.prompt ?? 'Challenge',
+                  style: UI.appText.copyWith(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.w900,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: size.height * 0.1,
+                ),
+                Text(
+                  no.toString(),
+                  style: UI.appText.copyWith(
+                    fontSize: 64.0,
+                    fontWeight: FontWeight.w900,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: size.height * 0.2,
+                ),
+                Text(
+                  '$time sec',
+                  style: UI.appText.copyWith(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: size.height * 0.05,
+                ),
+                MaterialButton(
+                  minWidth: size.width * 0.8,
+                  onPressed: () {
+                    timer.cancel();
+                    Future.delayed(
+                      Duration(seconds: 1),
+                    ).then(
+                      (value) => Get.back(),
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Submit',
+                      style: UI.appText.copyWith(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  animationDuration: Duration(milliseconds: 700),
+                  color: UI.appButtonColor,
+                  elevation: 6.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              widget.challenge?.prompt ?? 'Challenge',
-              style: UI.appText.copyWith(
-                fontSize: 30.0,
-                fontWeight: FontWeight.w900,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: size.height * 0.1,
-            ),
-            Text(
-              widget.challenge?.start.toString() ?? '0',
-              style: UI.appText.copyWith(
-                fontSize: 64.0,
-                fontWeight: FontWeight.w900,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: size.height * 0.2,
-            ),
-          ],
+          ),
         ),
       ),
     );
