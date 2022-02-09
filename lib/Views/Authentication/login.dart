d2d4a4b83004a7744ca29c930bf0c9fb740d4dc8
Sumit Pal <36818574+Sam21-39@users.noreply.github.com>
2022-02-09 08:21:50 +0530

    Update login.dart

--- lib/Views/Authentication/login.dart
+++ lib/Views/Authentication/login.dart
@@ -14,6 +14,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  var isLoading = false.obs;
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -41,53 +42,62 @@ class _LoginPageState extends State<LoginPage> {
               'assets/images/wally.png',
               height: size.height * 0.16,
             ),
-            SizedBox(
-              width: size.width * 0.8,
-              child: MaterialButton(
-                onPressed: () async {
-                  final auth = Auth.instance;
-                  final db = DataHandler.dataInstance;
-                  final result = await auth.signInWithGoogle();
-                  print(result);
-                  if (result != false) {
-                    final sp = await SharedPreferences.getInstance();
-                    sp.setBool('isLogged', true);
-                    db.initiateUserCreation();
-                    Get.off(
-                      () => DashBoardPage(
-                        index: 0,
-                      ),
-                      popGesture: false,
-                    );
-                    // Fluttertoast.showToast(
-                    //   msg: 'Some error occured. Try again later',
-                    // );
-                  } else {
-                    Get.snackbar(
-                      'Error',
-                      'Some Error occured. Try again later!',
-                    );
-                  }
-                },
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Image.asset(
-                      'assets/images/google.png',
-                      height: size.height * 0.05,
-                    ),
-                    Text(
-                      'Sign In with Google',
-                      style: UI.appText.copyWith(fontSize: 24.0),
-                    ),
-                  ],
-                ),
-                animationDuration: Duration(milliseconds: 700),
-                color: UI.appButtonColor,
-                elevation: 6.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12.0),
+            Obx(
+              () => SizedBox(
+                width: size.width * 0.8,
+                child: MaterialButton(
+                  onPressed: () async {
+                    isLoading.value = true;
+                    final auth = Auth.instance;
+                    final db = DataHandler.dataInstance;
+                    final result = await auth.signInWithGoogle();
+                    // print(result);
+
+                    if (result != false) {
+                      final sp = await SharedPreferences.getInstance();
+                      sp.setBool('isLogged', true);
+                      db.initiateUserCreation();
+                      Get.off(
+                        () => DashBoardPage(
+                          index: 0,
+                        ),
+                        popGesture: false,
+                      );
+                      // Fluttertoast.showToast(
+                      //   msg: 'Some error occured. Try again later',
+                      // );
+                    } else {
+                      isLoading.value = false;
+                      Get.snackbar(
+                        'Error',
+                        'Some Error occured. Try again later!',
+                      );
+                    }
+                  },
+                  padding: const EdgeInsets.all(8.0),
+                  child: isLoading.value
+                      ? CircularProgressIndicator(
+                          color: UI.appErrorColor,
+                        )
+                      : Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Image.asset(
+                              'assets/images/google.png',
+                              height: size.height * 0.05,
+                            ),
+                            Text(
+                              'Sign In with Google',
+                              style: UI.appText.copyWith(fontSize: 24.0),
+                            ),
+                          ],
+                        ),
+                  animationDuration: Duration(milliseconds: 700),
+                  color: UI.appButtonColor,
+                  elevation: 6.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
                 ),
               ),
             ),
